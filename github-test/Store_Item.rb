fa3e334e029b3f9c03552905a9db5784587d39cf
BanderSaleh <56854273+BanderSaleh@users.noreply.github.com>
2020-06-23 18:32:50 -0700

    Add files via upload

---
+++ github-test/Store_Item.rb
@@ -0,0 +1,34 @@
+class Material
+  attr_reader :color, :price, :location
+  attr_writer :price
+
+  def initialize(color, price, location)
+    @color = color
+    @price = price
+    @location = location
+  end
+
+  def color
+    p "#{@color}"
+  end
+
+  def price
+    p "#{@price}"
+  end
+  
+  def location
+    p "#{@location}"
+  end
+  
+  def print_info
+    p "here is the info"
+    p "#{@color} Is Worth #{@price} And Located At #{@location}"
+  end
+end
+
+item1 = Material.new("Gala Apple", "$1 a lb", "Produce")
+
+# item1.color
+# item1.price
+# item1.location
+item1.print_info
